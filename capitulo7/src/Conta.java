0547058e57ff22513f99ce199612c62edf3a4b1e
Witória Manuely <manuelycavalcante@gmail.com>
2018-01-04 20:01:06 -0300

    Questões do capitulo 7 da apostila

---
+++ capitulo7/src/Conta.java
@@ -0,0 +1,20 @@
+public class Conta {
+    protected double saldo;
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+    public void saca(double valor) {
+        this.saldo -= valor;
+    }
+    public double getSaldo() {
+        return this.saldo;
+    }
+    public void atualiza(double taxa) {
+        this.saldo += this.saldo * taxa;
+    }
+    public void mostra(){
+        System.out.println(getSaldo());
+    }
+
+}
